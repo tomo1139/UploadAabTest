bb36a2241e0866bdd028942eeaf6cb0305295643
gamma2278 <gamma2278@gmail.com>
2022-12-22 16:25:57 +0900

    no message

---
+++ app/src/main/java/develop/beta1139/uploadaabtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package develop.beta1139.uploadaabtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
